02de2aa0d98a4c720c86e07b72e8c435e1bdc5c0
Mohammad Nikanajm <md.nikanjam@gmail.com>
2024-12-30 09:50:10 +0330

    update

---
+++ EncoderSSI.cpp
@@ -0,0 +1,318 @@
+
+// #############################################################################################
+// Include libraries:
+
+#include "EncoderSSI.h"
+
+using namespace EncoderSSI_Namespace;
+
+// ###############################################################################################
+// Include private macros.
+
+#define _2PI         6.283185307            // 2*3.14159 = 2*pi
+
+// ###############################################################################################
+// LPF and LPFLimit class:
+
+LPF::LPF()
+{
+    parameters.FRQ = 0;
+    _alpha = 1;
+
+    clear();
+}
+
+double LPF::updateByTime(const double &input, const double &dt)
+{
+    if( (dt <= 0) || (parameters.FRQ < 0) )
+    {
+        // Not update output.;
+        return output;
+    }
+
+    if(parameters.FRQ == 0)
+    {
+        output = input;
+        return output;
+    }
+
+    _alpha = 1.0/(1.0 + 1.0 / (_2PI * parameters.FRQ * dt));
+    output = (1.0 - _alpha) * output + _alpha * input;
+
+    return output;
+}
+
+double LPF::updateByFrequency(const double &input, const double &frq)
+{
+    if( (frq <= 0) || (parameters.FRQ < 0) )
+    {
+        // Not update output.;
+        return output;
+    }
+
+    if(parameters.FRQ == 0)
+    {
+        output = input;
+        return output;
+    }
+
+    _alpha = 1.0/(1.0 + frq / (_2PI * parameters.FRQ));
+
+    output = (1.0 - _alpha) * output + _alpha * input;
+
+    return true;
+}
+
+bool LPF::setFrequency(const double &frq)
+{
+    if(frq < 0)
+    {
+        return false;
+    }
+
+    parameters.FRQ = frq;
+
+    return true;
+}
+
+void LPF::setOutputDirect(const double &data)
+{
+    output = data;
+}
+
+void LPF::clear(void)
+{
+    output = 0;
+}
+
+// ###############################################################################################
+// LotusEncoderSSI class
+
+EncoderSSI::EncoderSSI()
+{    
+    _virtualOffset = 0;
+
+    parameters.COMMUNICATION_MODE = 0;
+    parameters.CLK_GPIO_PORT = nullptr;
+    parameters.DATA_GPIO_PORT = nullptr;
+    parameters.CLK_GPIO_PIN = GPIO_PIN_0;
+    parameters.DATA_GPIO_PIN = GPIO_PIN_0;
+    parameters.TIMER = nullptr;
+    parameters.HSPI = nullptr;
+    parameters.FLTA = 0;
+    parameters.FLTR = 0;
+    parameters.RESOLUTION = 17;
+    parameters.RATE_FRQ = 0;
+    parameters.GEAR_RATIO = 1;
+    parameters.POSRAW_OFFSET_DEG = 0;
+    parameters.MAP_ENA = false;
+    parameters.MAP_MAX = 0;
+    parameters.MAP_MIN = 0;
+
+    value.posDeg = 0;
+    value.posRawDeg = 0;
+    value.posRawStep = 0;
+    value.velDegSec = 0;
+
+    value.revolutionCounter = 0;
+    _posRawDegPast = 0;
+}
+
+EncoderSSI::~EncoderSSI()
+{
+    clean();
+}
+
+bool EncoderSSI::init(void)
+{
+    if(_checkParameters() == false)
+    {
+        return false;
+    }
+
+    _virtualOffset = 0;
+
+    if(parameters.HSPI->Instance == NULL)
+    {
+        errorMessage = "Error EncoderSSI: SPI handle instance is null.";
+        return false;
+    }
+
+    if (HAL_SPI_Init(parameters.HSPI) != HAL_OK) 
+    {
+        errorMessage = "Error EncoderSSI: HAL_SPI_Init is not succeeded.";
+        return false;
+    }
+        
+    if(!_LPFR.setFrequency(parameters.FLTR) || !_LPFR.setFrequency(parameters.FLTR))
+    {
+        errorMessage = "Error LotusEncoderSSI: Filter frequency for rate is not correct value.";
+        return false;
+    }
+
+    if(!_LPFA.setFrequency(parameters.FLTA) || !_LPFA.setFrequency(parameters.FLTA))
+    {
+        errorMessage = "Error LotusEncoderSSI: Filter frequency for angle is not correct value.";
+        return false;
+    }
+
+    return true;
+}
+
+void EncoderSSI::_readAngle_spi(void)
+{
+    // Reset value of reading data
+    char encoder_read_data[3] = {0, 0, 0};
+
+    // Transfer Dummy data for generate clock 
+    if(parameters.RESOLUTION > 16)
+    { 
+        HAL_SPI_TransmitReceive(parameters.HSPI, (uint8_t*)encoder_read_data, (uint8_t*)encoder_read_data, 3, HAL_MAX_DELAY);    
+    }
+    else if(parameters.RESOLUTION > 8)
+    {
+        HAL_SPI_TransmitReceive(parameters.HSPI, (uint8_t*)encoder_read_data, (uint8_t*)encoder_read_data, 2, HAL_MAX_DELAY);
+    }
+    else
+    {
+        HAL_SPI_TransmitReceive(parameters.HSPI, (uint8_t*)encoder_read_data, (uint8_t*)encoder_read_data, 1, HAL_MAX_DELAY);
+    }
+
+    encoder_read_data[0] = encoder_read_data[0] & 0b01111111;       // Clear first bit data for start communication.
+
+    uint32_t rawDataStep = 0;
+    double rawDataDeg = 0;
+
+    rawDataStep = (  (((uint32_t)encoder_read_data[0]) << 16) | ((uint32_t)encoder_read_data[1] << 8) | (((uint32_t)encoder_read_data[2])) );
+    rawDataStep = (rawDataStep >> (24 - parameters.RESOLUTION - 1));   // Clear extera bits.
+
+    rawDataDeg = (double)rawDataStep / pow(2, (double)parameters.RESOLUTION) * 360.0;
+    
+    value.posRawStep = rawDataStep;
+    _posRawDegPast = value.posRawDeg;
+    value.posRawDeg = rawDataDeg;
+
+    if( (value.posRawDeg - _posRawDegPast) < -180 ) 
+    {
+        value.revolutionCounter++;
+    }
+    else if( ((value.posRawDeg - _posRawDegPast) > 180) && (_posRawDegPast != 0))
+    {
+        value.revolutionCounter--;
+    }
+}
+
+double EncoderSSI::_mapAngleToCustomRange(double angle, double minRange, double maxRange) 
+{ 
+    double range = maxRange - minRange; 
+    double temp = fmod(angle - minRange, range); // Shift angle to start at minRange 
+    if (temp < 0) 
+    { 
+        temp += range; 
+    } return temp + minRange; 
+}
+
+void EncoderSSI::update(void)
+{
+    uint64_t T_now = parameters.TIMER->micros();
+    double dt = (double)(T_now - _T) / 1000000.0;
+    _T = T_now;
+
+    if(dt <= 0)
+    {
+        return;
+    }
+
+    _readAngle_spi();
+
+    double temp = ( (value.posRawDeg - parameters.POSRAW_OFFSET_DEG + value.revolutionCounter * 360.0) * parameters.GEAR_RATIO );
+    _LPFA.updateByTime(temp, dt);
+    if(parameters.RATE_FRQ > 0)
+    {
+        double dt_rate = ((double)(T_now - _TRate) / 1000000.0);
+
+        if( dt_rate >= (1.0 / parameters.RATE_FRQ) )
+        {
+            value.velDegSec = (_LPFA.output - _posForRate) / dt_rate;
+            value.velDegSec =  _LPFR.updateByTime(value.velDegSec, dt_rate);
+            _TRate = T_now;
+            _posForRate = _LPFA.output;
+        }
+    }
+    else
+    {
+        value.velDegSec = (_LPFA.output - value.posDeg) / dt;
+        value.velDegSec =  _LPFR.updateByTime(value.velDegSec, dt);
+    }
+    
+    value.posDeg = _LPFA.output;  
+
+    if(parameters.MAP_ENA == true)
+    {
+        value.posDeg = _mapAngleToCustomRange(value.posDeg, parameters.MAP_MIN, parameters.MAP_MAX);
+    }
+    
+}
+
+bool EncoderSSI::setPresetValueDeg(double data)
+{
+    parameters.TIMER->delayMicroseconds(100);
+    switch(parameters.COMMUNICATION_MODE)
+    {
+        case 0:
+            _readAngle_spi();
+        break;
+        case 1:
+
+        break;
+    }
+
+    parameters.POSRAW_OFFSET_DEG = value.posRawDeg - data;
+
+    return true;
+}
+
+void EncoderSSI::clean(void)
+{
+    for(int i = 0; i <= 1; i++)
+    {
+        value.posDeg = 0;
+        value.posRawDeg = 0;
+        value.posRawStep = 0;
+        value.velDegSec = 0;
+
+        value.revolutionCounter = 0;
+        _posRawDegPast = 0;
+    }
+}
+
+bool EncoderSSI::_checkParameters(void)
+{
+    bool state;
+
+    state = state && (parameters.FLTA >= 0) && (parameters.FLTA >= 0) &&
+                     (parameters.FLTR >= 0) && (parameters.FLTR >= 0) &&
+                     (parameters.RESOLUTION > 0) && (parameters.RESOLUTION <= 23) &&
+                     (parameters.RESOLUTION > 0) && (parameters.RESOLUTION <= 23);
+
+
+
+    if(state == false)
+    {
+        errorMessage = "Error LotusEncoderSSI: One or some parameters are not correct.";
+    }
+
+    for(int i = 0; i <= 1; ++i)
+    {
+        if( (parameters.MAP_ENA == true) && (parameters.MAP_MAX == parameters.MAP_MIN) )
+        {
+            errorMessage = "Error LotusEncoderSSI: mapMax and mapMin can not equal when mapENA is true.";
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+
---
+++ EncoderSSI.h
@@ -0,0 +1,319 @@
+#pragma once
+
+// ##############################################################################################
+// MCU Select:
+
+#include "mcu_select.h"
+
+/*
+    If there is not exist mcu_select.h at beside of this header file, Create it and put this bellow following content. 
+    Then select your desired MCU that want work with.
+*/
+// ----------------------------------------------------------------
+// mcu_select.h file:
+
+// Define the target MCU family here
+// Uncomment the desired MCU family definition below:
+
+// #define STM32F1
+// #define STM32F4
+// #define STM32H7
+
+// ----------------------------------------------------------------
+
+// ########################################################################
+// Include libraries:
+
+#if defined(STM32F1)
+#include "stm32f1xx_hal.h"      // HAL library for STM32F1 series
+#elif defined(STM32F4)
+#include "stm32f4xx_hal.h"      // HAL library for STM32F4 series
+#elif defined(STM32H7)
+#include "stm32h7xx_hal.h"      // HAL library for STM32H7 series
+#else
+#error "Unsupported MCU family. Please define a valid target (e.g., STM32F1, STM32F4, STM32H7)."
+#endif
+
+#include <string>       // Include the standard string library for error handling and messages
+#include <math.h>
+#include "TimerControl.h"
+
+// #######################################################################################
+// LPF class:
+
+/**
+ * @namespace EncoderSSI_Namespace
+ * @brief The namespace for safety protection.
+ *  */ 
+namespace EncoderSSI_Namespace
+{
+    /**
+     * @class LPF
+     * @brief Low pass filter class
+     */
+    class LPF
+    {
+        public:
+            
+            /**
+             * @brief Parameters structure.
+             */
+            struct ParametersStructure
+            {
+                /// @brief Cut off frequency parameter of low pass filter. [Hz]. The value of 0 means it is disabled. 
+                double FRQ;                   
+            }parameters;
+
+            /**
+             * @brief Default constructor. Init some variables.
+             */
+            LPF();
+
+            /**
+             * @brief Buffer memory for output of lowpass filter.
+             */
+            double output;              
+
+            /**
+             * @brief Low pass filter updatation. Calculate output signal relative to input signal and time step.
+             * Store value of filtered output signal.
+             * @param input: Value of input signal.
+             * @param dt: Time step duration. [sec].
+             * @return output of filter.
+             */
+            virtual double updateByTime(const double &input, const double &dt);
+
+            /**
+             * @brief Low pass filter updatation. Calculate output signal relative to input signal and frequency of update step.
+             * Store value of filtered output signal.
+             * @param input: Value of input signal.
+             * @param frq: Frequency updatation step. [Hz].
+             * @return output of filter
+             */
+            virtual double updateByFrequency(const double &input, const double &frq);
+
+            /**
+             * @brief Set Cut off frequency of low pass filter.
+             * @param frq: Cut off frequency. [Hz]
+             * @return true if successed.
+             *  */  
+            bool setFrequency(const double &frq);
+
+            /**
+             * @brief Set output directely without filter.
+             * Use this function for init value of output signal.
+             */
+            void setOutputDirect(const double &data);
+
+            /**
+             * @brief Clear any buffer on variables.
+             */
+            void clear(void); 
+
+        protected:
+
+            /**
+             * @brief Low Pass filter gain.
+             * @note - Filter in continuse space: G(s) = output/input = 1/(T*s + 1)
+             * @note - T: Time constant for lowpass filter.
+             * @note -  Filter cut off frequency (frq_c) = 1/(2pi*T)
+             * @note -  alpha gain = 1/( 1 + 1/(dt/T) ) = 1/( 1 + 1/(2*pi*frq_c*dt) )
+             * @note -  dt: Time step duration. [sec]
+             * @note -  output_(n) = (1 - alpha) * output_(n-1) + alpha * input_(n)
+             *  */ 
+            double _alpha;           
+
+    };
+
+}
+
+// #####################################################################################
+// LotusEncoderSSI class 
+
+/**
+ * @class EncoderSSI
+ * @brief EncoderSSI class.
+ *  */ 
+class EncoderSSI
+{
+    public:
+
+        /// @brief  Last error occurred for object.
+        std::string errorMessage;               
+
+        /**
+         * @struct ParametersStructure
+         * @brief Parameters structure.
+         */
+        struct ParametersStructure
+        {
+            /**
+             * @brief SPI handle
+             */
+             SPI_HandleTypeDef* HSPI;
+
+            TimerControl* TIMER;
+
+            uint8_t COMMUNICATION_MODE;
+
+            GPIO_TypeDef* CLK_GPIO_PORT;
+
+            GPIO_TypeDef* DATA_GPIO_PORT;
+
+            uint16_t CLK_GPIO_PIN;
+
+            uint16_t DATA_GPIO_PIN;
+
+            /**
+             * @brief Cut off frequency parameter of low pass filter for rate value. [Hz]
+             * @note The value of 0 means it is disabled. 
+             *  */ 
+            double FLTR;   
+
+            /**
+             * @brief Cut off frequency parameter of low pass filter for angle value. [Hz].
+             * @note The value of 0 means it is disabled.   
+             *  */               
+            double FLTA;     
+
+            bool MAP_ENA;
+
+            /**
+             * @brief The value of Map for minimum angle calculations. [deg]
+             */
+            double MAP_MIN;
+
+            /**
+             * @brief The value of Map for maximum angle calculations. [deg]
+             */
+            double MAP_MAX;
+            
+            /**
+             * @brief Encoder single turn resolution.
+             * @note It must be lower than 23 bit single turn.
+             *  */                  
+            uint8_t RESOLUTION;         
+
+            /**
+             * @brief Offset value channel 1 and 2 for raw angle measurement. [deg]
+             * @note Initial value is 0.
+             *  */ 
+            double POSRAW_OFFSET_DEG;            
+
+            /**
+             * @brief Gearbox ratio for angle measurement.
+             * @note posDeg = posDeg * gearRatio.
+             *  */  
+            double GEAR_RATIO;
+
+            /**
+             * @brief The frequency of rate measurements derived from position data. [Hz]
+             * @note The value of 0 means it is disabled.
+             */
+            double RATE_FRQ;   
+        }parameters;
+
+        /**
+         * @struct ValuesStructure
+         * @brief Values structure.
+         */
+        struct ValuesStructure
+        {
+            /**
+             * @brief Raw encoder value for channel 1 and 2. [-]
+             * @note Max value depend on resolution.
+             *  */ 
+            uint32_t posRawStep;                 
+
+             /**
+             * @brief Encoder shaft angle for channel 1 and 2. [deg].
+             * @note posRawDeg = (posRawStep / max_posRawStep) * 360
+             *  */ 
+            double posRawDeg;     
+
+            /**
+             * @brief Position angle for channel 1 and 2. [deg]
+             * @note posDeg = (posRawDeg - posRawOffsetDeg + _revolutionCounter * 360.0) * gearRatio.
+             *  */ 
+            double posDeg;                     
+
+            /**
+             * @brief Velocity for channel 1 and 2. [deg/s].
+             * @note velDegSec = d(posDeg)/dt 
+             *  */      
+            double velDegSec;   
+
+            ///! @brief Number of cross over from overflow for posRawStep since object created.                
+            int32_t revolutionCounter;             
+        }value;
+
+        /** 
+         * @brief EncoderSSI constructor. Set encoder resolution and spi mode. Not apply setting.
+         * @note init() method needs after this for apply setting on hardware.
+        */      
+        EncoderSSI();
+
+        /// @brief Destructor.
+        ~EncoderSSI();
+
+        /** 
+         * @brief Init/Setup then Apply setting on hardware. Start LotusEncoderSSI action.
+         * @return true if succeeded.
+        */
+        bool init(void);
+
+        /**
+         * @brief Read and update values.
+         */
+        void update(void);
+
+        /**
+         * @brief Set the PresetValue object that contains the desired absolute preset value. Writing this object executes a preset.
+         * The encoder internally calculates a preset offset value which is being stored in a non-volatile memory
+         * (no store command via CoE object 0x1010 required).
+         * @param value is desired step value for position at current position.  
+         * @return true if successed.
+         * @warning Use this function after setting direction rotation of encoder. Otherwise it maybe not correct set.
+         *  */  
+        bool setPresetValueDeg(double value);
+
+        /**
+         * @brief Clean setting on hardware. Stop LotusEncoderSSI action. 
+         * Clear any buffer on output data.
+        */
+        void clean(void);
+
+    private:
+
+        double _virtualOffset;
+
+        double _posRawDegPast;                   ///! @brief Buffer for angleInput
+
+        uint64_t _T;                                ///! @brief [us]. Time at update method. used for derivative and low pass filter calculations.
+
+        uint64_t _TRate;
+
+        double _posForRate;
+
+        EncoderSSI_Namespace::LPF _LPFR;   ///! @brief Low pass filter for rate value channel 1.
+
+        EncoderSSI_Namespace::LPF _LPFA;   ///! @brief Low pass filter for angle value channel 1.
+
+        /** 
+         * @brief Read raw value for certain channel. Calculate and update values of posRawStep and posRawDeg.
+         * @param channel: channel number. Can be 1 or 2.   
+        */
+        void _readAngle_spi(void);
+
+        void _readAngle_gpio(void);
+
+        /**
+         * @brief Check parameters validation.
+         * @return true if succeeded.
+         */
+        bool _checkParameters(void);
+
+        double _mapAngleToCustomRange(double angle, double minRange, double maxRange);
+
+};
+
